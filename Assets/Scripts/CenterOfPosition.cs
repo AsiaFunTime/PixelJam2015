e71a950a66f56105c9ca07f763af368bd7e9a319
Yao Hong Chun <yhchun1992@hotmail.com>
2015-08-29 09:31:29 +1200

    Initial Commit

---
+++ Assets/Scripts/CenterOfPosition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterOfPosition : MonoBehaviour
+{
+    public int ruler = 1;
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 centroid = new Vector3(0, 0, 0);
+        GameObject[] units = GameObject.FindGameObjectsWithTag("Unit" + ruler);
+        foreach (GameObject unit in units)
+        {
+            centroid += unit.transform.position;
+        }
+        centroid /= units.Length;
+
+        transform.position = new Vector3(centroid.x, 0, centroid.z);
+        print(units.Length);
+    }
+}
---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	public UnitBehavior unit;
+
+    public float CurrentSpeed = 0f;
+	// Use this for initialization
+	void Start () {
+		unit = GetComponent<UnitBehavior> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (unit.Ruler == 0)
+			return;
+
+		float h = Input.GetAxis ("Horizontal" + unit.Ruler);
+		float v = Input.GetAxis ("Vertical" + unit.Ruler);
+
+        CurrentSpeed = CurrentSpeed + (Time.deltaTime * )
+
+        transform.Translate(Vector3.back * v * unit.Acceleration * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.right * h * unit.Acceleration * Time.deltaTime, Space.World);
+	}
+}
