e96ef9dfe9df58738180069402f283af9c12645d
zacharyf1991 <zachary.freiberg@gmail.com>
2015-08-29 17:49:37 +1200

    Added audio updates

--- Assets/Scripts/AudioManagerScript.cs
+++ Assets/Scripts/AudioManagerScript.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class AudioManagerScript : MonoBehaviour {
 
-	public string[] audioName;
-	public AudioClip[] audioClip;
-	public bool clipFound;
-	public AudioClip[] grunts = new AudioClip[6];
-	public AudioClip[] attacks = new AudioClip[6];
-	public AudioClip[] dies = new AudioClip[6];
-	public AudioClip[] cheers = new AudioClip[3];
+
+	// Generic
+	AudioClip[] grunts = new AudioClip[6];
+	AudioClip[] attacks = new AudioClip[6];
+	AudioClip[] dies = new AudioClip[6];
+	AudioClip[] cheers = new AudioClip[3];
+
 
 	// Arrow
-	public AudioClip[] arrowShoot = new AudioClip[6];
-	public AudioClip[] arrowCollide = new AudioClip[6];
+	AudioClip[] arrowShoot = new AudioClip[6];
+	AudioClip[] arrowCollide = new AudioClip[6];
+
+	AudioClip horseGallop = new AudioClip();
 
 	public GameObject gameManager;
 
@@ -57,7 +59,22 @@ public class AudioManagerScript : MonoBehaviour {
 		cheers[2] = Resources.Load ("Audio/SoundEffects/Metal/man_victory_03") as AudioClip;
 
 		// Arrow
+		arrowShoot[0] = Resources.Load ("Audio/SoundEffects/Metal/bow_shoot_01") as AudioClip;
+		arrowShoot[1] = Resources.Load ("Audio/SoundEffects/Metal/bow_shoot_02") as AudioClip;
+		arrowShoot[2] = Resources.Load ("Audio/SoundEffects/Metal/bow_shoot_03") as AudioClip;
+		arrowShoot[3] = Resources.Load ("Audio/SoundEffects/Metal/bow_shoot_04") as AudioClip;
+		arrowShoot[4] = Resources.Load ("Audio/SoundEffects/Metal/bow_shoot_05") as AudioClip;
+		arrowShoot[5] = Resources.Load ("Audio/SoundEffects/Metal/bow_shoot_06") as AudioClip;
 
+		arrowCollide[0] = Resources.Load ("Audio/SoundEffects/Metal/arrow_ground_01") as AudioClip;
+		arrowCollide[1] = Resources.Load ("Audio/SoundEffects/Metal/arrow_ground_02") as AudioClip;
+		arrowCollide[2] = Resources.Load ("Audio/SoundEffects/Metal/arrow_ground_03") as AudioClip;
+		arrowCollide[3] = Resources.Load ("Audio/SoundEffects/Metal/arrow_ground_04") as AudioClip;
+		arrowCollide[4] = Resources.Load ("Audio/SoundEffects/Metal/arrow_ground_05") as AudioClip;
+		arrowCollide[5] = Resources.Load ("Audio/SoundEffects/Metal/arrow_ground_06") as AudioClip;
+
+		// Horse
+		horseGallop = Resources.Load ("Audio/SoundEffects/Horse/cavalry_charge") as AudioClip;
 
 
 	}
@@ -92,9 +109,33 @@ public class AudioManagerScript : MonoBehaviour {
 			int i = Random.Range( 0, 2 );
 			audioSource.clip = cheers[i] ;
 		}
+		else if ( clipName == "arrowShoot" )
+		{
+			int i = Random.Range( 0, 5 );
+			audioSource.clip = arrowShoot[i] ;
+		}
+		else if ( clipName == "arrowCollide" )
+		{
+			int i = Random.Range( 0, 5 );
+			audioSource.clip = arrowCollide[i] ;
+		}
+
+		else if ( clipName == "horseGallop" )
+		{
+			audioSource.volume = 0.5f;
+			audioSource.clip = horseGallop ;
+		}
 		audioSource.Play();
 		StartCoroutine(WaitThenDestroy(audioSource.clip.length, audioSource));
 	
+	}
+
+	public void StopAllAudio ()
+	{
+		AudioSource[] audioSources = GetComponents<AudioSource>();
+		foreach(AudioSource audio in audioSources){
+			audio.Stop();
+		}
 	}
 
 	public IEnumerator WaitThenDestroy(float waitTime, AudioSource audioSource){
